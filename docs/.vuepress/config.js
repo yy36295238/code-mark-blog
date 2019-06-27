f190a07e81afe20ceb3a9ad8377f20c43f7355f6
YangYu <36295238@qq.com>
2019-06-27 13:36:49 +0800

    更新

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    title: 'Code-Mark',
+    description: '代码标记',
+    themeConfig: {
+        sidebarDepth: 2,
+        sidebar: {
+            '/java/': [
+                {
+                    title: 'JAVA',
+                    collapsable: false,
+                    children: [
+                        '/java/stream',
+                        '/java/string_utils',
+                    ]
+                },
+            ],
+
+        }
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,8 @@
+---
+home: true
+# heroImage: /kot-logo.png
+actionText: Get Started →
+actionLink: /java/
+features:
+footer: MIT Licensed | Copyright © 2019-present Evan You
+---
